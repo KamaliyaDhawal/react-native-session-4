5ae51e6af8f44c0955d46c748ba84c2340cb0e58
dhaval.kamaliya <dhaval.kamaliya@solutionanalysts.com>
2020-02-01 16:53:14 +0530

    [Add] get data api

--- screen/Home.js
+++ screen/Home.js
@@ -5,6 +5,25 @@ export default function Home({navigation}) {
     console.log('navigation', navigation);
     const user = navigation.getParam('result');
     console.log(user);
+    fetch('http://35.160.197.175:3006/api/v1/recipe/cooking-list',
+    {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${user.token}`
+        },
+    })
+    .then((response) => {
+        // console.log(response);
+        if(response.status === 200) {
+            return(response.json());
+        } else {
+            Alert.alert('Error', "Invalid email or password"); 
+        }
+    })
+    .then((result) => {
+        console.log(result);
+        navigation.navigate('Home', {result});
+    })
     return(
         <View>
             <Text>
